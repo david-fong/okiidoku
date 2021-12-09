1d8723c03f64b7829a8ebe7647cbf55e2897bfe5
david-fong <davidfong19@gmail.com>
2021-12-08 19:04:43 -0800

    switch from std_dev in canon relabel to joint prob

--- cpp/src/solvent_lib/equiv/canon.cpp
+++ cpp/src/solvent_lib/equiv/canon.cpp
@@ -68,9 +68,9 @@ namespace solvent::lib::equiv {
 		grid_vec_t<O> operator()(void);
 
 	 private:
-		grid_mtx_t<O> input_;
+		grid_mtx_t<O> grid_;
 		void relabel_(void) noexcept;
-		void movement_(void);
+		void reposition_(void);
 	};
 
 
@@ -84,23 +84,23 @@ namespace solvent::lib::equiv {
 
 	template<Order O>
 	Canonicalizer<O>::Canonicalizer(const grid_vec_t<O>& input) {
-		input_ = lib::grid_vec2mtx<O>(input);
+		grid_ = lib::grid_vec2mtx<O>(input);
 	}
 
 
 	template<Order O>
 	grid_vec_t<O> Canonicalizer<O>::operator()(void) {
 		relabel_();
-		movement_();
+		reposition_();
 
 		// const std::vector<print::print_grid_t> grid_accessors = {
 		// 	print::print_grid_t([this](std::ostream& os, uint16_t coord) {
-		// 		os << ' '; print::val2str(os, O, input_[coord/O2][coord%O2]);
+		// 		os << ' '; print::val2str(os, O, grid_[coord/O2][coord%O2]);
 		// 	}),
 		// };
 		// print::pretty(std::cout, O, grid_accessors);
 
-		grid_vec_t<O> ret = grid_mtx2vec<O>(input_);
+		grid_vec_t<O> ret = grid_mtx2vec<O>(grid_);
 		return ret;
 	}
 
@@ -114,7 +114,7 @@ namespace solvent::lib::equiv {
 
 		Choosing two labels A and B and swapping the corresponding rows and
 		the corresponding columns produces the table generated when instances
-		of A and B in `input_` are swapped.
+		of A and B in `grid_` are swapped.
 
 		The probability of values A and B being in the same atom within a
 		block is `p(n) = 2/(o+1)` (simplified from `2(o-1)/(o^2-1)`). The
@@ -125,9 +125,7 @@ namespace solvent::lib::equiv {
 		- Each row (or column) sums to O2*(2*(O1-1)).
 		- The maximum possible value at any coordinate is O2.
 		- The expected value at any non-diagonal coordinate is 2*O2/(O1+1).
-			Simplified from (O2*(2*(O1-1)))/(O2-1). Using law of large numbers.
 			Examples by order: 2: 2.67,  3: 4.50,  4: 6.40,  5: 8.33
-			It can also be calculated in the standard way by using p(n).
 		*/
 		std::array<std::array<ord2_t, O2>, O2> counts = {{0}};
 
@@ -137,11 +135,11 @@ namespace solvent::lib::equiv {
 				for (ord1_t cell_i = 0; cell_i < O1 - 1; cell_i++) {
 					for (ord1_t cell_j = cell_i + 1; cell_j < O1; cell_j++) {
 						{ // boxrow
-							const ord2_t label_i = input_[line][atom+cell_i], label_j = input_[line][atom+cell_j];
+							const ord2_t label_i = grid_[line][atom+cell_i], label_j = grid_[line][atom+cell_j];
 							counts[label_i][label_j]++; counts[label_j][label_i]++;
 						}
 						{ // boxcol
-							const ord2_t label_i = input_[atom+cell_i][line], label_j = input_[atom+cell_j][line];
+							const ord2_t label_i = grid_[atom+cell_i][line], label_j = grid_[atom+cell_j][line];
 							counts[label_i][label_j]++; counts[label_j][label_i]++;
 						}
 					}
@@ -156,42 +154,36 @@ namespace solvent::lib::equiv {
 		// The reduction calculation's result must not depend on the ordering
 		// of the counts entries. It should encapsulate a label's degree of
 		// preference to being in an atom with some labels more than others.
-		// The specific reduction below is the standard deviation.
+		// The specific reduction below is a naive joint probability.
 		for (ord2_t label = 0; label < O2; label++) {
-			static constexpr std::array<double, O2+1> std_dev = [](){
+			static constexpr std::array<double, O2+1> prob = [](){
 				std::array<double, O2+1> arr;
 				for (unsigned count = 0; count < O2+1; count++) {
-					static constexpr double expected = static_cast<double>(2*O2)/(O1+1);
-					const double p_binomial = static_cast<double>(
+					arr[count] = static_cast<double>(
 						n_choose_r(O2, count) * std::pow(2, count) * std::pow(O1-1, O2-count)
 					) / std::pow(O1+1, O2);
-					arr[count] = p_binomial * std::pow(static_cast<double>(count) - expected, 2);
 				}
 				return arr;
 			}();
 			const double sort_basis = std::transform_reduce(
 				std::execution::par_unseq,
 				counts[label].cbegin(), counts[label].cend(),
-				0.0, std::plus<double>(), [](const ord2_t count) {
-					return std_dev[count];
-				}
+				1.0, std::multiplies<double>(), [](const ord2_t count) { return prob[count]; }
 			);
 			canon2orig_label[label] = SortMapEntry {
-				.orig {label},
-				.sort_basis {sort_basis},
+				.orig = label,
+				.sort_basis = sort_basis,
 			};
-			// Note: skipped taking the square root of .sort_basis (the standard
-			// deviation), since doing it does not change the outcome of sorting.
 		}
 		// Make the lower-valued labels "play favourites":
-		std::ranges::sort(canon2orig_label, std::ranges::greater(), &SortMapEntry::sort_basis);
+		std::ranges::sort(canon2orig_label, std::ranges::less(), &SortMapEntry::sort_basis);
 		// std::cout << "\n"; for (auto e : canon2orig_label) { std::cout << e.sort_basis << "  "; }
 
 		std::array<ord2_t, O2> label_map = {0};
 		for (ord2_t i = 0; i < O2; i++) {
 			label_map[canon2orig_label[i].orig] = i;
 		}
-		for (auto& row : input_) {
+		for (auto& row : grid_) {
 			for (auto& e : row) {
 				e = label_map[e];
 			}
@@ -275,37 +267,37 @@ namespace solvent::lib::equiv {
 	// TODO test the tie-breaking strength of this implementation.
 	// if it fails, look into using the same-atom-relationship table
 	template<Order O>
-	void Canonicalizer<O>::movement_() {
-		const GridSlide grid_slide = GridSlide::build(input_);
+	void Canonicalizer<O>::reposition_() {
+		const GridSlide grid_slide = GridSlide::build(grid_);
 		const GridSlide transposed_grid_slide = [this](){
-			decltype(input_) transposed_input;
+			decltype(grid_) transposed_input;
 			for (ord2_t i = 0; i < O2; i++) {
 				for (ord2_t j = 0; j < O2; j++) {
-					transposed_input[i][j] = input_[j][i];
+					transposed_input[i][j] = grid_[j][i];
 				}
 			}
 			return GridSlide::build(transposed_input);
 		}();
 
-		decltype(input_) canon_input = {O2};
+		decltype(grid_) canon_input = {O2};
 		for (ord2_t canon_row = 0; canon_row < O2; canon_row++) {
 			const auto& r_chute = grid_slide[canon_row/O1];
 			const ord2_t orig_row = (O1*r_chute.orig_chute) + r_chute[canon_row%O1].orig_blkline;
 			for (ord2_t canon_col = 0; canon_col < O2; canon_col++) {
 				const auto& c_chute = transposed_grid_slide[canon_col/O1];
 				const ord2_t orig_col = (O1*c_chute.orig_chute) + c_chute[canon_col%O1].orig_blkline;
-				canon_input[canon_row][canon_col] = input_[orig_row][orig_col];
-				// canon_input[orig_row][orig_col] = input_[canon_row][canon_col];
+				canon_input[canon_row][canon_col] = grid_[orig_row][orig_col];
+				// canon_input[orig_row][orig_col] = grid_[canon_row][canon_col];
 			}
 		}
 		if (transposed_grid_slide < grid_slide) {
 			for (ord2_t i = 0; i < O2; i++) {
 				for (ord2_t j = 0; j < O2; j++) {
-					input_[i][j] = canon_input[j][i];
+					grid_[i][j] = canon_input[j][i];
 				}
 			}
 		} else {
-			input_ = canon_input;
+			grid_ = canon_input;
 		}
 	}
 
